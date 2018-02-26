1754fe27684d53fc26980e05c08b7e9e3369787a
Aaron Li <dumpling.d.m@gmail.com>
2018-02-26 09:24:01 -0500

    Key based scrolling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
+var d = new Date();
 var sections = [].slice.call(document.getElementsByClassName("fullPage"));
-var sectionLinks = [].slice.call(document.getElementsByClassName("buttonExtend"));
+var buttonAnimationBackgrounds = [].slice.call(document.getElementsByClassName("buttonExtend"));
+var buttonLinks = [].slice.call(document.getElementsByClassName("navbarLink"));
 sections.splice(0,1); // first element is title page
 document.onscroll = function(){
   var scrollPos = document.documentElement.scrollTop-window.innerHeight/2.0;
   var focusedWindow = Math.floor(scrollPos/window.innerHeight);
   for(var i=0;i<sections.length;i++){
     if(i===focusedWindow){
-      sectionLinks[i].style.animationDelay = "0s";
-      sectionLinks[i].style.animationName = "focus";
-      sectionLinks[i].style.animationPlayState = "running";
+      buttonAnimationBackgrounds[i].style.animationDelay = "0s";
+      buttonAnimationBackgrounds[i].style.animationName = "focus";
+      buttonAnimationBackgrounds[i].style.animationPlayState = "running";
     }
     else{
-      var otherLink = sectionLinks[i]
+      var otherLink = buttonAnimationBackgrounds[i]
       otherLink.style.animationName = "unfocus";
+    }
+  }
+}
+var currentSection = Math.floor( document.documentElement.scrollTop/window.innerHeight);
+var lastCalled = d.getTime();
+document.onkeydown = function(event){
+  d = new Date();
+  var keyDown = event.keyCode||event.which;
+  if(d.getTime()-lastCalled>500){
+    if(keyDown === 38){
+      currentSection = Math.max(0,--currentSection);
+      buttonLinks[currentSection].click();
+      lastCalled = d.getTime();
+    }
+    else if(keyDown === 40){
+      currentSection = Math.min(buttonLinks.length-1,++currentSection);
+      buttonLinks[currentSection].click();
+      lastCalled = d.getTime();
     }
   }
 }
