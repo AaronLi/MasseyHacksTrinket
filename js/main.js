82970432d1cecf024659dc2fd26fb2e139bc37d8
Aaron Li <dumpling.d.m@gmail.com>
2018-02-26 09:40:59 -0500

    arrow key navigation+scrolling

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ var sections = [].slice.call(document.getElementsByClassName("fullPage"));
 var buttonAnimationBackgrounds = [].slice.call(document.getElementsByClassName("buttonExtend"));
 var buttonLinks = [].slice.call(document.getElementsByClassName("navbarLink"));
 sections.splice(0,1); // first element is title page
+var currentSection = ( document.documentElement.scrollTop/window.innerHeight);
 document.onscroll = function(){
   var scrollPos = document.documentElement.scrollTop-window.innerHeight/2.0;
   var focusedWindow = Math.floor(scrollPos/window.innerHeight);
+  currentSection = Math.min(Math.max(( document.documentElement.scrollTop/window.innerHeight),0),buttonLinks.length);
   for(var i=0;i<sections.length;i++){
     if(i===focusedWindow){
       buttonAnimationBackgrounds[i].style.animationDelay = "0s";
@@ -18,18 +20,19 @@ document.onscroll = function(){
     }
   }
 }
-var currentSection = Math.floor( document.documentElement.scrollTop/window.innerHeight);
 var lastCalled = d.getTime();
 document.onkeydown = function(event){
   d = new Date();
   var keyDown = event.keyCode||event.which;
-  if(d.getTime()-lastCalled>500){
+  if(d.getTime()-lastCalled>550){
     if(keyDown === 38){
+      currentSection = Math.ceil(currentSection);
       currentSection = Math.max(0,--currentSection);
       buttonLinks[currentSection].click();
       lastCalled = d.getTime();
     }
     else if(keyDown === 40){
+      currentSection = Math.floor(currentSection);
       currentSection = Math.min(buttonLinks.length-1,++currentSection);
       buttonLinks[currentSection].click();
       lastCalled = d.getTime();
